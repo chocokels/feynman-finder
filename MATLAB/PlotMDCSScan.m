d8a1aa047acda216bdb670ef78c8d7e5d26b91d8
chocokels <kelseymb12@gmail.com>
2023-10-06 15:48:40 -0700

    Changed how axes are defined I think?
    These changes were made a little while ago, so I don't remember why exactly. I think it added axes labels, limits on imagesc, a factor of 2pi on the plotting axis, and additional outputs to the function.

--- MATLAB/PlotMDCSScan.m
+++ MATLAB/PlotMDCSScan.m
@@ -10,7 +10,7 @@
 % Output:
 %   res:  Outputs figure number used, or 0 on failure
 
-function fignum = PlotMDCSScan(ts,dat,four,compx)
+function [dat,ax1,ax2,fignum] = PlotMDCSScan(ts,dat,four,compx)
 
 axs = find(size(dat) > 1);
 
@@ -23,37 +23,49 @@ end
 ax1 = ts{axs(1)};
 ax2 = ts{axs(2)};
 
+dat = squeeze(dat);
+
+if four
+    dat = ifftshift(ifftshift(ifft(ifft(dat,length(ax1),1),length(ax2),2),1),2);
+    
+    ax1 = 2*pi/(ax1(2)-ax1(1)) * ((0:length(ax1)-1)/length(ax1)-1/2);
+    ax2 = 2*pi/(ax2(2)-ax2(1)) * ((0:length(ax2)-1)/length(ax2)-1/2);
+end
+
+if compx == 0
+    dat = abs(dat);
+    lims = [0 max(dat,[],'all')];
+elseif compx == 1
+    dat = real(dat);
+    lims = [-1 1] * max(abs(dat),[],'all');
+elseif compx == 2
+    dat = imag(dat);
+    lims = [-1 1] * max(abs(dat),[],'all');
+elseif compx == 3
+    dat = angle(dat);
+    lims = [-pi pi];
+end
+
+imagesc(ax2,ax1,dat,lims);
+set(gca,'YDir','normal');
+
 if length(ts) == 3
     if ~four
         labs = {'\tau','T','t'};
     else
         labs = {'\omega_\tau','\omega_T','\omega_t'};
     end
-    ax1lab = labs{axs(1)};
-    ax2lab = labs{axs(2)};
+    xlabel(labs{axs(1)});
+    ylabel(labs{axs(2)});
+else
+    if ~four
+        xlabel(['t_' num2str(ax(2))]);
+        ylabel(['t_' num2str(ax(1))]);
+    else
+        xlabel(['\omega_{t' num2str(axs(2)) '}']);
+        ylabel(['\omega_{t' num2str(axs(1)) '}']);
+    end
 end
-
-dat = squeeze(dat);
-
-if four
-    dat = ifftshift(ifftshift(ifft(ifft(dat,length(ax1),1),length(ax2),2),1),2);
-    
-    ax1 = 1/(ax1(2)-ax1(1)) * ((0:length(ax1)-1)/length(ax1)-1/2);
-    ax2 = 1/(ax2(2)-ax2(1)) * ((0:length(ax2)-1)/length(ax2)-1/2);
-end
-
-if compx == 0
-    dat = abs(dat);
-elseif compx == 1
-    dat = real(dat);
-elseif compx == 2
-    dat = imag(dat);
-end
-
-imagesc(ax2,ax1,dat);
-xlabel(ax2lab);
-ylabel(ax1lab);
-set(gca,'YDir','normal');
 
 fignum = get(gcf,'Number');
 
